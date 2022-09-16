8f22db94740d4d4a20eff4f5a3c4a29f835460b0
lsh9295 <lsh929500@gmail.com>
2022-09-16 18:10:50 +0900

    add: 구글맵 연동

---
+++ app/src/main/java/com/example/mudang2/ApiKey.kt
@@ -0,0 +1,10 @@
+package com.example.mudang2
+
+class ApiKey {
+    companion object {
+        // 인코딩 키
+        const val API_KEY =  "extnbCu4SI01Yg05Le4eoKY2lAmc2Jq9%2F6btYg1YDLuEpm5wDiin%2B2ujdppr1nfMO3r0Gv05E9UKJrgovNeEIQ%3D%3D"
+        // 디코딩 키
+        // const val API_KEY = "extnbCu4SI01Yg05Le4eoKY2lAmc2Jq9/6btYg1YDLuEpm5wDiin+2ujdppr1nfMO3r0Gv05E9UKJrgovNeEIQ==
+    }
+}
